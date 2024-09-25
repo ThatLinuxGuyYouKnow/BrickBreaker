0a54f63cd71498b1bdc37a6c697b11434ee8317e
Emmanuels-code <arocket04@gmail.com>
2024-09-25 11:15:47 +0100

    building main game screen

---
+++ lib/mainGameScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white);
+  }
+}
