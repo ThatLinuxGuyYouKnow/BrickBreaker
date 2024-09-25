4254b1a408b72631161e7c1816bbca0a0f803d76
Emmanuels-code <arocket04@gmail.com>
2024-09-25 11:20:29 +0100

    building main game screen

--- lib/mainGameScreen.dart
+++ lib/mainGameScreen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
-class GameScreen extends StatefulWidget {
+class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
 
   Widget build(BuildContext context) {
-    return Container(color: Colors.white);
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Container(
+        color: Colors.white,
+        child: Column(
+          children: [SizedBox(height: screenHeight * 5)],
+        ));
   }
 }
