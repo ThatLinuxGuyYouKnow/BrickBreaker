86d08ce0c0ae0731de2a3d8c9c0e157a66b4e1be
Emmanuels-code <arocket04@gmail.com>
2024-09-25 20:40:31 +0100

    moved alignment logic to paddle

--- lib/game-assets/paddle.dart
+++ lib/game-assets/paddle.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Paddle extends StatefulWidget {
-  Paddle({super.key, this.alignmentX = 1.0});
+  Paddle({super.key, this.alignmentX = 0.0});
   double alignmentX;
 
   @override
@@ -9,22 +9,29 @@ class Paddle extends StatefulWidget {
 }
 
 class _PaddleState extends State<Paddle> {
+  double Xalignment = 0.0;
+
+  @override
   Widget build(BuildContext context) {
-    double Xalignment = 100.0;
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
     return GestureDetector(
       onHorizontalDragUpdate: (details) {
-        Xalignment = details.globalPosition.dx;
-        print('x alignmrnt is ' + Xalignment.toString());
+        setState(() {
+          // Convert drag position into alignment value between -1.0 and 1.0
+          Xalignment = (details.globalPosition.dx / screenWidth) * 2 - 1;
+          print('X alignment is ' + Xalignment.toString());
+        });
       },
-      child: Container(
+      child: Align(
         alignment: Alignment(Xalignment, 1.0),
-        height: screenHeight * .04,
-        width: screenWidth * 0.2,
-        decoration: BoxDecoration(
-            color: Colors.blue, borderRadius: BorderRadius.circular(2)),
+        child: Container(
+          height: screenHeight * .04,
+          width: screenWidth * 0.2,
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(2)),
+        ),
       ),
     );
   }
