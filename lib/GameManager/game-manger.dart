155c6d2959eb3a651acc687d24009052ab374955
Emmanuels-code <arocket04@gmail.com>
2024-09-27 23:14:10 +0100

    editing movement handlers and logic

--- lib/GameManager/game-manger.dart
+++ lib/GameManager/game-manger.dart
@@ -1,90 +1,25 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
 class GameManager {
-  final Function(Offset) updateBallPosition;
-  final Function(double) updatePaddlePosition;
-  final Size screenSize;
+  final double paddleX;
+  Offset _ballPosition = const Offset(100, 100); // Ball position
 
-  Offset ballPosition;
-  Offset ballVelocity;
-  double paddleX;
-  double paddleWidth;
-  double paddleHeight;
+  // Constructor initializes paddleX and ball position
+  GameManager(this.paddleX);
 
-  late Timer gameLoop;
+  // Getters to access ballX and ballY
+  double get ballX => _ballPosition.dx;
+  double get ballY => _ballPosition.dy;
 
-  GameManager({
-    required this.updateBallPosition,
-    required this.updatePaddlePosition,
-    required this.screenSize,
-    this.ballPosition = const Offset(100, 100),
-    this.ballVelocity = const Offset(3, 3),
-    this.paddleX = 0,
-    this.paddleWidth = 80,
-    this.paddleHeight = 20,
-  });
-
-  void startGame() {
-    print('game started');
-    gameLoop = Timer.periodic(const Duration(milliseconds: 16), _update);
+  void moveBall(double ballXIncrement, double ballYIncrement) {
+    _ballPosition = _ballPosition.translate(ballXIncrement, ballYIncrement);
   }
 
-  void _update(Timer timer) {
-    _moveBall();
-    _checkWallCollision();
-    _checkPaddleCollision();
-    updateBallPosition(ballPosition);
+  // Setter to manually update the ball's position
+  set ballPosition(Offset newPosition) {
+    _ballPosition = newPosition;
   }
 
-  void _moveBall() {
-    ballPosition += ballVelocity;
-  }
-
-  void _checkWallCollision() {
-    if (ballPosition.dx <= 0 || ballPosition.dx >= screenSize.width - 20) {
-      ballVelocity = Offset(-ballVelocity.dx, ballVelocity.dy);
-    }
-    if (ballPosition.dy <= 0) {
-      ballVelocity = Offset(ballVelocity.dx, -ballVelocity.dy);
-    }
-    if (ballPosition.dy >= screenSize.height - 20) {
-      // Game over logic here
-      gameLoop.cancel();
-      print('Game Over!');
-    }
-  }
-
-  void _checkPaddleCollision() {
-    final paddleRect = Rect.fromLTWH(
-      paddleX,
-      screenSize.height - paddleHeight - 20,
-      paddleWidth,
-      paddleHeight,
-    );
-
-    final ballRect = Rect.fromLTWH(
-      ballPosition.dx,
-      ballPosition.dy,
-      20,
-      20,
-    );
-
-    if (ballRect.overlaps(paddleRect)) {
-      ballVelocity = Offset(ballVelocity.dx, -ballVelocity.dy.abs());
-
-      final hitPoint =
-          (ballPosition.dx + 10 - paddleRect.left) / paddleRect.width;
-      final angleChange = (hitPoint - 0.5) * 2; // -1 to 1
-      ballVelocity = Offset(ballVelocity.dx + angleChange, ballVelocity.dy);
-
-      final speed = 6.0; // Constant speed
-      ballVelocity = ballVelocity / ballVelocity.distance * speed;
-    }
-  }
-
-  void updatePaddle(double newX) {
-    paddleX = newX;
-    updatePaddlePosition(paddleX);
-  }
+  // Getter to access the full ball position as an Offset
+  Offset get ballPosition => _ballPosition;
 }
--- lib/mainGameScreen.dart
+++ lib/mainGameScreen.dart
@@ -16,32 +16,9 @@ class _BrickBreakerGameState extends State<BrickBreakerGame> {
   double paddleX = 0;
 
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final size = MediaQuery.of(context).size;
-      gameManager = GameManager(
-        updateBallPosition: (position) =>
-            setState(() => ballPosition = position),
-        updatePaddlePosition: (x) => setState(() => paddleX = x),
-        screenSize: size,
-      );
-      gameManager.startGame();
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
-        onHorizontalDragUpdate: (details) {
-          if (gameManager != null) {
-            final screenWidth = MediaQuery.of(context).size.width;
-            final newX = details.globalPosition.dx - (screenWidth * 0.1);
-            gameManager
-                .updatePaddle(newX.clamp(0, screenWidth - (screenWidth * 0.2)));
-          }
-        },
         child: Container(
           color: Colors.black,
           child: Stack(
