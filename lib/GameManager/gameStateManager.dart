a36f134529d24bd7db2dd45d66a084b19ececc84
Emmanuels-code <arocket04@gmail.com>
2024-09-28 15:04:06 +0100

    context in GameManager

---
+++ lib/GameManager/gameStateManager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GameStateManager extends StatelessWidget {
+  GameStateManager({super.key});
+
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [Container()],
+    );
+  }
+}
